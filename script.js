7c2100566d784af6aea3c7d10095c75bd8ff1a4e
dcheever <cheeverdanny@gmail.com>
2021-03-21 21:40:03 -0500

    working 2

--- script.js
+++ script.js
@@ -1,65 +1,82 @@
-// Assignment code here
+var lowerCaseCharCodes = arrayFromLowToHigh(97,122)
+var uppercaseCharCodes = arrayFromLowToHigh(65, 90)
+var numberCarCharCodes = arrayFromLowToHigh(48, 57)
+var symbolCharCodes = arrayFromLowToHigh(33, 47).concat(
+    arrayFromLowToHigh(58, 64)
+).concat(
+    arrayFromLowToHigh(91, 96)
+).concat(
+    arrayFromLowToHigh(123, 126)
+)
 
-// character codes
-var upperCase = arrayFromLowToHigh(65, 90);
-var lowerCase = arrayFromLowToHigh(97, 122);
-var numberCase = arrayFromLowToHigh(48, 57);
-var symbolCase = arrayfromLowToHigh(33,47)
-  .concat(arrayfromLowToHigh(58, 64))
-  .concat(arrayFromLowToHigh(91, 96))
-  .concat(arrayFromLowToHigh(123, 126));
-  
-  // generate password
-  let writePassword = (
-    characterLength,
-    includeUppercase,
-    includeNumbers,
-    includeSymbols
-  ) => {
-    let charCodes = upperCase;
-    if (includeUppercase) charCodes = charCodes.concat(upperCase);
-    if (includeSymbols) charCodes = charCodes.concat(symbolCase);
-    if (includeNumbers) charCodes = charCodes.concat(numberCase);
-    var passwordCharacters = [];
-    for (let i = 0; i < characterAmount; i++) {
-      var characterCode =
-      charCodes[Math.floor(Math.random() * charCodes.length)];
-      passwordCharacters.push(String.fromCharCode(characterCode));
-    }
-    return passwordCharacters.join("");
-  };
+characterAmountRange.addEventListener("input", syncCharacterAmount)
+characterAmountNumber.addEventListener("input", syncCharacterAmount)
 
-  // generate characters
-  function arrayFromLowToHigh(low, high) {
-    var array = [];
-    for (let i = low; i <= high; i++) {
-      array.push(i);
-    }
-    return array;
-  };
+function onClick(e) {
+    e.preventDefault()
 
-  
-  var aray = ["a", "b", "c", "d", "e"];
-  var blankString = ("");
+    var characterAmountNumber = document.getElementById ("characterAmountNumber")
+    var includeUppercaseElement = document.getElementById ("upperCaseCharacters")
+    var includeNumbersElement = document.getElementById ("numbersCharacters")
+    var includeSymbolsElement = document.getElementById ("symbolsCharacters")
+    var passwordText = document.getElementById("password")
+    var cardBody = document.getElementById("card-body")
+    cardBody.style.display = "block";
 
-  for (var i = 0; i < aray.length; i++) {
-    aray[Math.floor(Math.random(aray.length))];
-    blanketString.push(String.fromaray);
-  };
-
-  
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+    var characterAmount = characterAmountNumber.value
+    var includeUpperCase = includeUppercaseElement.checked
+    var includeNumbers = includeNumbersElement.checked
+    var includeSymbols = includeSymbolsElement.checked
+    var password = generatePassword(characterAmount, includeUpperCase, includeNumbers, includeSymbols)
+    passwordText.innerText = password
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function toggle(e) {
+    e.preventDefault()
+    var inputs = document.getElementById ("inputs")
+    var generate = document.getElementById ("generate")
+    var hideshow = document.getElementById ("hideshow")
+        inputs.style.display = "block";
+        generate.style.display = "block";
+        hideshow.style.display = "none";
+        
+    
+    }
+
+var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", onClick);
+var hideShow = document.querySelector("#hideshow");
+hideShow.addEventListener("click", toggle);
+
+
+function syncCharacterAmount(e) {
+    var value = e.target.value
+    characterAmountRange.value = value
+    characterAmountNumber.value = value
+};
+
+function generatePassword(characterAmount, includeUpperCase, includeNumbers, includeSymbols) {
+    console.log(characterAmount + includeUpperCase + includeNumbers + includeSymbols)
+
+    var charCodes = lowerCaseCharCodes
+    if (includeUpperCase) charCodes = charCodes.concat(uppercaseCharCodes)
+    if (includeNumbers) charCodes = charCodes.concat(numberCarCharCodes)
+    if (includeSymbols) charCodes = charCodes.concat(symbolCharCodes)
+    
+    var passwordCharacters = []
+    for (var i = 0; i < characterAmount; i++) {
+        var characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+        passwordCharacters.push(String.fromCharCode(characterCode))
+    }
+    console.log(passwordCharacters)
+    return passwordCharacters.join("")
+};
+
+function arrayFromLowToHigh(low, high) {
+    var array = [];
+    for (var i = low; i <= high; i++) {
+        array.push(i)
+    }
+    return array;
+};
